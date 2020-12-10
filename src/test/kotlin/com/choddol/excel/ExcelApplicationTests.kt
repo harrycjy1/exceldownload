1dca74a1d40d9c3b1bfd1882b58a2041152f4dfe
jinyoung <harrycjy123@gmail.com>
2020-12-10 14:53:58 +0900

    init

---
+++ src/test/kotlin/com/choddol/excel/ExcelApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.choddol.excel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExcelApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
